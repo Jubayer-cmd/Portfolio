5d66a73389c28bd3ec79adfc26d2900983534138
jubayer <sheikhabujubayer@gmail.com>
2022-06-10 22:04:31 +0600

    added contact

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <a
+        href="https://www.linkedin.com/in/sheikh-abu-jubayer-457282177/"
+        target="_blank"
+        alt="LinkedIn"
+        rel="noreferrer"
+      >
+        <BsLinkedin />
+      </a>
     </div>
     <div>
-      <FaFacebookF />
+      <a
+        href="https://github.com/Jubayer-cmd"
+        target="_blank"
+        alt="GitHub"
+        rel="noreferrer"
+      >
+        <BsGithub />
+      </a>
     </div>
     <div>
-      <BsInstagram />
+      <a
+        href="https://www.facebook.com/skjubayer.islam/"
+        target="_blank"
+        alt="Facebook"
+        rel="noreferrer"
+      >
+        <FaFacebookF />
+      </a>
     </div>
   </div>
 );
--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
-import { AppWrap } from '../../wrapper';
-import { images } from '../../constants';
-import './Header.scss';
+import { images } from "../../constants";
+import { AppWrap } from "../../wrapper";
+import "./Header.scss";
 
 const scaleVariants = {
   whileInView: {
@@ -11,7 +10,7 @@ const scaleVariants = {
     opacity: [0, 1],
     transition: {
       duration: 1,
-      ease: 'easeInOut',
+      ease: "easeInOut",
     },
   },
 };
@@ -28,12 +27,12 @@ const Header = () => (
           <span>👋</span>
           <div style={{ marginLeft: 20 }}>
             <p className="p-text">Hello, I am</p>
-            <h1 className="head-text">Micael</h1>
+            <h1 className="head-text">Jubayer</h1>
           </div>
         </div>
 
         <div className="tag-cmp app__flex">
-          <p className="p-text">Web Developer</p>
+          <p className="p-text">Full-Stack Developer</p>
           <p className="p-text">Freelancer</p>
         </div>
       </div>
@@ -47,7 +46,7 @@ const Header = () => (
       <img src={images.profile} alt="profile_bg" />
       <motion.img
         whileInView={{ scale: [0, 1] }}
-        transition={{ duration: 1, ease: 'easeInOut' }}
+        transition={{ duration: 1, ease: "easeInOut" }}
         src={images.circle}
         alt="profile_circle"
         className="overlay_circle"
@@ -59,7 +58,7 @@ const Header = () => (
       whileInView={scaleVariants.whileInView}
       className="app__header-circles"
     >
-      {[images.flutter, images.redux, images.sass].map((circle, index) => (
+      {[images.javascript, images.react, images.html].map((circle, index) => (
         <div className="circle-cmp app__flex" key={`circle-${index}`}>
           <img src={circle} alt="profile_bg" />
         </div>
@@ -68,4 +67,4 @@ const Header = () => (
   </div>
 );
 
-export default AppWrap(Header, 'home');
+export default AppWrap(Header, "home");
